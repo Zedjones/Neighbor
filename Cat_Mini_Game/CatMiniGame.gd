0b07b0e6683129f06f0950bc429ffda11b005e27
lestherreynoso <lhreynos@gmail.com>
2018-05-26 12:04:04 -0400

    started cat spawning

--- Cat_Mini_Game/CatMiniGame.gd
+++ Cat_Mini_Game/CatMiniGame.gd
@@ -1,5 +1,6 @@
-extends Area2D
+extends Node
 
+export (PackedScene) var Cat
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
@@ -7,17 +8,11 @@ extends Area2D
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
-	$AnimationPlayer.play("run")
+	var cat = Cat.instance()
+	add_child(cat)
 	pass
 
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
-
-
-func _on_OrangeCat_input_event(viewport, event, shape_idx):
- #   if event.type == InputEvent.MOUSE_BUTTON \
-  #  and event.button_index == BUTTON_LEFT \
-   # and event.is_pressed():
-     print("Clicked")
