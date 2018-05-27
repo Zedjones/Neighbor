114582d264e3966389a3ad16e8d75e31615604fe
Nicholas Jones <dojoman19@gmail.com>
2018-05-27 14:59:35 -0400

    Finished mini-game interface

---
+++ MiniGame.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var DialogueChoices = preload("res://GlobalData.gd").DialogueChoices
+
+func set_points(dialogue_choices):
+	pass
+	
+func is_game_over():
+	return false
+	
+func get_score():
+	return 0
