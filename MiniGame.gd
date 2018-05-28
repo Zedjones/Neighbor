4c83fa97906468b8655f4b4562a4fa08d289d241
Nicholas Jones <dojoman19@gmail.com>
2018-05-28 15:41:45 -0400

    Added comments to explain mini-game interface

--- MiniGame.gd
+++ MiniGame.gd
@@ -3,11 +3,16 @@ extends Node
 var DialogueChoices = preload("res://GlobalData.gd").DialogueChoices
 var GameOutcomes = preload("res://GlobalData.gd").GameOutcomes
 
+# GameManager or Character will call this to talk to mini-game
+# @param dialogue_choices - a DialogueChoices enum item 
 func set_points(dialogue_choices):
 	pass
 	
+# Whether or not the game is complete 
 func is_game_over():
 	return false
 	
+# GameManager will call this to get the outcome from the mini-game
+# @return - a GameOutcomes enum item
 func get_score():
-	return 0
+	return GameOutcomes.BETTER
