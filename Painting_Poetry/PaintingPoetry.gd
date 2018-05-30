d1c437080ec5fbe7e3f7034a68fe25b7c83e6694
Unknown <jxa2576@g.rit.edu>
2018-05-30 15:04:37 -0400

    Basic game loop done
    Still needs scoring, and ending condition, and a bit of clean up

--- Painting_Poetry/PaintingPoetry.gd
+++ Painting_Poetry/PaintingPoetry.gd
@@ -2,36 +2,101 @@ extends Node
 
 export (PackedScene) var Letter
 
+signal kill
+
+var spacing = 20
+
+var sun = ["the world grew darker", "as the sun dove", "into the ocean"]
+var star = ["bursting light", "a star flew", "dissipating into the night"]
+var fire = ["wild flames", "birthed by a howling bonfire", "lighting up the woods"]
+var candle = ["flickering away", "the candle melted", "leaving a puddle"]
+
+var currentLineNum = 0
+
+var currentLines = {"sun": "", "star": "", "fire": "", "candle": ""}
+
+var currentLetterNum = {"sun": 0, "star": 0, "fire": 0, "candle": 0}
+
+var poemLetterPos = Vector2(525, 250)
+
 func _ready():
 	$Paintings/Sun.frame = 0
 	$Paintings/Star.frame = 4
 	$Paintings/Fire.frame = 8
 	$Paintings/Candle.frame = 12
+	
+	setup(currentLineNum)
+	
 	pass
 
 func _process(delta):
 	
-	if $Paintings/Sun.frame < 3:
-		$Paintings/Sun.frame += 1
-	else:
-		$Paintings/Sun.frame = 0
+	if Input.is_action_pressed("ui_up"):
+		emit_signal("kill")
 	
-	
-	if $Paintings/Star.frame < 7:
-		$Paintings/Star.frame += 1
-	else:
-		$Paintings/Star.frame = 4
-	
-	
-	if $Paintings/Fire.frame < 11:
-		$Paintings/Fire.frame += 1
-	else:
-		$Paintings/Fire.frame = 8
-	
-	
-	if $Paintings/Candle.frame < 15:
-		$Paintings/Candle.frame += 1
-	else:
-		$Paintings/Candle.frame = 12
+	for key in currentLines:
+		write_poem(key)
 	
 	pass
+
+func current_lines(num):
+	currentLines["sun"] = sun[num].to_upper()
+	currentLines["star"] = star[num].to_upper()
+	currentLines["fire"] = fire[num].to_upper()
+	currentLines["candle"] = candle[num].to_upper()
+
+func setup(lineNum):
+	
+	if currentLineNum > 2:
+		currentLineNum = 0
+	
+	current_lines(currentLineNum)
+	currentLineNum += 1
+	
+	var linePos = Vector2(10, 250)
+	
+	for key in currentLines:
+		spawn_line(currentLines[key], linePos)
+		linePos.y += 75
+
+func spawn_line(line, pos):
+	
+	var currentLetterPos = 0
+	
+	while currentLetterPos < line.length():
+		var letter = Letter.instance()
+		add_child(letter)
+		letter.spawn_letter(line.substr(currentLetterPos,1))
+		connect("kill", letter, "kill_letter")
+		#letter.add_color_override("font_color", Color(0,0,0))
+		currentLetterPos += 1
+		letter.rect_position = pos
+		pos = Vector2(pos.x + spacing, pos.y)
+		
+func write_poem(poem):
+	if Input.is_action_pressed(currentLines[poem].substr(currentLetterNum[poem], 1)):
+		#connect new signal for game reset not just line reset
+		print("MATCH")
+		currentLetterNum[poem] += 1
+	elif currentLines[poem].substr(currentLetterNum[poem], 1) == " ":
+		currentLetterNum[poem] += 1
+	
+	if currentLetterNum[poem] == currentLines[poem].length():
+		print("Spawn")
+		currentLetterNum[poem] = 0
+		spawn_poem(poem)
+		poemLetterPos = Vector2(525, poemLetterPos.y + 75)
+		emit_signal("kill")
+		setup(currentLineNum)
+
+func spawn_poem(poem):
+	var currentLetterPos = 0
+	
+	while currentLetterPos < currentLines[poem].length():
+		var letter = Letter.instance()
+		add_child(letter)
+		letter.spawn_letter(currentLines[poem].substr(currentLetterPos, 1))
+		letter.add_color_override("font_color", Color(0,0,0))
+		letter.rect_position = poemLetterPos
+		poemLetterPos = Vector2(poemLetterPos.x + spacing, poemLetterPos.y)
+		currentLetterPos += 1
