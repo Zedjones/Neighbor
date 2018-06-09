313aea668b584ed52882f3754564e402766130b1
lestherreynoso <lhreynos@gmail.com>
2018-06-09 14:50:21 -0400

    Forgot the sky portion of the tween.

--- Overworld.gd
+++ Overworld.gd
@@ -4,6 +4,11 @@ extends Node
 # var a = 2
 # var b = "textvar"
 
+export (Color) var dayTime
+export (Color) var nightTime
+
+var tweenDuration = 3
+
 func _ready():
 	# Called every time the node is added to the scene.
 	# Initialization here
@@ -17,7 +22,7 @@ func _ready():
 
 func _on_PlayButton_pressed():
 	print($Camera2D.position)
-	$Tween.interpolate_property($Camera2D, "position", $Camera2D.position, 
-		Vector2($Camera2D.position.x, get_viewport().get_size().y/2 + 51), 3, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$Tween.interpolate_property($Camera2D, "position", $Camera2D.position, Vector2($Camera2D.position.x, get_viewport().get_size().y/2 + 51), tweenDuration, Tween.TRANS_QUAD, Tween.EASE_OUT)
+	$Tween.interpolate_property($Sky, "modulate", dayTime, nightTime, tweenDuration, Tween.TRANS_QUAD, Tween.EASE_OUT)
 	$Tween.start()
 
