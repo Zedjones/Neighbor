43ea2353d81183106ee84649d7216c7019c03bec
Nicholas Jones <dojoman19@gmail.com>
2018-06-17 16:53:56 -0400

    Finished basis for the game manager

--- GameManager.gd
+++ GameManager.gd
@@ -1,19 +1,55 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-enum DialogueChoices {Worst, Okay, Better, Best}
+var DialogueChoices = preload("res://GlobalData.gd").DialogueChoices
+var GameOutcomes = preload("res://GlobalData.gd").GameOutcomes
 var curr_character
+var scene_manager
+var time_manager
+var dialogue_manager
+var in_mini_game = false
+var mini_game
+var dialogue_choice
+
+const HAPPINESS_BASE = 5
+const BETTER_MULT = 1.2
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
+	
+func _process(delta):
+	if in_mini_game:
+		if mini_game.is_game_over():
+			in_mini_game = false
+			var score = mini_game.get_score()
+			match score:
+				GameOutcomes.BETTER:
+					curr_character.adjust_happiness(dialogue_choice*BETTER_MULT*HAPPINESS_BASE)
+				GameOutcomes.WORSE:
+					curr_character.adjust_happiness(dialogue_choice*HAPPINESS_BASE)
+			mini_game = null
+			dialogue_choice = null
 
 func set_curr_character(character):
-	curr_character = character
-	
-func launch_minigame(dialogue_choices):
-	curr_character.launch_minigame(choice)
+	if curr_character != character:
+		curr_character = character
+		if character != null:
+			#dialogue_manager.prompt(character)
+			pass
+
+func handle_dialogue(dialogue_choice):
+	mini_game = curr_character.mini_game
+	match dialogue_choice:
+		DialogueChoices.Worst:
+			pass
+		DialogueChoices.Okay:
+			mini_game.set_points(DialogueChoices.Okay)
+			scene_manager.loadScene(mini_game)
+			in_mini_game = true
+			self.dialogue_choice = dialogue_choice
+		DialogueChoices.Better:
+			curr_character.adjust_happiness(DialogueChoices.Better*HAPPINESS_BASE)
+		DialogueChoices.Best:
+			mini_game.set_points(DialogueChoices.Best)
+			scene_manager.loadScene(mini_game)
+			in_mini_game = true
+			self.dialogue_choice = dialogue_choice
--- NPC.gd
+++ NPC.gd
@@ -8,13 +8,17 @@ var leftSpeed = -0.1
 var currentSpeed = rightSpeed
 var motion = Vector2()
 var startingXPos
+onready var game_manager = get_node("/root/GameManager")
 export (PackedScene) var mini_game
 signal player_entered
+signal player_exited
 export (float) var happiness
 
 
 func _ready():
 	distanceTimer = randi()%51+1
+	connect("player_entered", game_manager, "set_curr_character")
+	connect("player_exited", game_manager, "set_curr_character")
 	startingXPos = position.x
 	pass
 
@@ -44,10 +48,16 @@ func UpdateDirectionAndMovement():
 	motion.x += currentSpeed
 	pass
 
+# Set GameManager current character to the character and 
+# call the dialogue blip if it hasn't already been called
 func _on_Interactive_Object_activated():
-	emit_signal("player_entered")
+	emit_signal("player_entered", self)
 	
 # GameManager will call this to adjust happiness after dialogue
 # @param points: a float with the number of points to add
 func adjust_happiness(points):
 	happiness += points
+
+# Set GameManager current character to null 
+func _on_IO_exited():
+	emit_signal("player_exited", null)
