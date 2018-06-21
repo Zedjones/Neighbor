0103a2d5a23b29740c9c5b8724f5ee370dbbf2ea
lestherreynoso <lhreynos@gmail.com>
2018-06-20 21:46:17 -0400

    Updated cat mini game to work full screen size and Scene manager

---
+++ SceneManager.gd
@@ -0,0 +1,27 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var currentScene
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	pass
+	
+func load_scene(scene):	
+	var instance = scene.instance()
+	var world = get_node("/root/World")
+	#world.get_tree().paused = true
+	world.add_child(instance)
+
+	pass
+	
+
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
