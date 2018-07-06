66991396f816501a1fbbb791a8d5b29696748829
lestherreynoso <lhreynos@gmail.com>
2018-07-06 15:12:44 -0400

    camera scrolling

--- Main Area/Camera2D.gd
+++ Main Area/Camera2D.gd
@@ -3,15 +3,17 @@ extends Camera2D
 # class member variables go here, for example:
 # var a = 2
 # var b = "textvar"
-export (float) var zoomStep = 0.25
+export (float) var targetZoomStep = 0.25
 var followPlayer = false
 var targetZoom
 var tweenDuration = 3
 var tween
-
+var maxZoom
+var minZoom = Vector2(.12, .12)
 
 func _ready():
 	#tween = get_node("../Tween")
+	maxZoom = get_zoom()
 	pass
 
 
@@ -27,25 +29,44 @@ func _input(event):
 	var initPosCam = false
 	var initPosMouse = false
 	var initPosNode = false
-	
+	var zoomStep = targetZoomStep
 	var zoomDelta = get_zoom()
+	
+	print(minZoom)
+	
 	if event is InputEventMouseButton:
 		print("Mouse Click/Unclick at: ", event.position)
 		if(event.button_index == BUTTON_WHEEL_UP):
-			print("Button_Wheel_Up: ", event.position)
+#			print("Button_Wheel_Up: ", event.position)
+			
+			if(zoomDelta[0] + zoomStep > maxZoom[0]):
+				zoomStep = clamp(zoomStep, 0, maxZoom[0] - zoomDelta[0])
+			if(zoomDelta[1] + zoomStep > maxZoom[1]):
+				zoomStep = clamp(zoomStep, 0, maxZoom[1] - zoomDelta[1])
+			print(zoomStep)
+				
 			zoomDelta[0] = zoomDelta[0] + zoomStep
 			zoomDelta[1] = zoomDelta[1] + zoomStep
+			
 		if(event.button_index == BUTTON_WHEEL_DOWN):
 			print("Button_Wheel_Down: ", event.position)
-			if(zoomDelta[0] - zoomStep > 0 && zoomDelta[1] - zoomStep > 0):
-	            zoomDelta[0] = zoomDelta[0] - zoomStep
-	            zoomDelta[1] = zoomDelta[1] - zoomStep
+#			if(zoomDelta[0] - zoomStep > 0 && zoomDelta[1] - zoomStep > 0):
+
+			if(zoomDelta[0] - zoomStep < minZoom[0]):
+				zoomStep = clamp(zoomStep, 0, zoomDelta[0] - minZoom[0])
+			if(zoomDelta[1] - zoomStep < minZoom[1]):
+				zoomStep = clamp(zoomStep, 0, zoomDelta[1] - minZoom[1] )
+			print(zoomStep)
+            
+			zoomDelta[0] = zoomDelta[0] - zoomStep
+			zoomDelta[1] = zoomDelta[1] - zoomStep
+			
 	
 		targetZoom = zoomDelta
 		reset_tween()
 			
-	elif event is InputEventMouseMotion:
-		print("Mouse Motion at: ", event.position)
+#	elif event is InputEventMouseMotion:
+#		print("Mouse Motion at: ", event.position)
 
 #	set_zoom(zoom)
 	
